6015f8815d6b3e0372564a46108116af69dd3037
Script.ez <96830819+Scriptez1@users.noreply.github.com>
2026-01-26 12:51:46 +0300

    Update RedX_Library.lua

--- RedX_Library.lua
+++ RedX_Library.lua
@@ -241,7 +241,7 @@ function RedX.new(title)
     -- Minimize button
     local minimizeBtn = Instance.new("TextButton", headerBar)
     minimizeBtn.Size = UDim2.new(0,32,0,32)
-    minimizeBtn.Position = UDim2.new(1,-140,0.5,-16)
+    minimizeBtn.Position = UDim2.new(1,-120,0.5,-16)
     minimizeBtn.BackgroundColor3 = Color3.fromRGB(40,40,40)
     minimizeBtn.Text = "−"
     minimizeBtn.Font = Enum.Font.GothamBold
