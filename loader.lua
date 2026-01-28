c2d793d726f0205c43a50c3036b9bb7f7951e69b
Script.ez <96830819+Scriptez1@users.noreply.github.com>
2026-01-28 14:07:04 +0300

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,5 +1,6 @@
 if game.PlaceId == 2753915549 or game.PlaceId == 4442272183 or game.PlaceId == 7449423635 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Scriptez1/RedXRobloxScripts/main/bloxfruit.lua"))()
+    print("Blox Fruits loaded")
     return
 else
     print("Unknown")
