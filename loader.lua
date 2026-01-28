34dc017add119643bdadffd2375f056523340ba6
Script.ez <96830819+Scriptez1@users.noreply.github.com>
2026-01-28 14:00:58 +0300

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,4 +1,7 @@
 if game.PlaceId == 2753915549 or game.PlaceId == 4442272183 or game.PlaceId == 7449423635 then
-     loadstring(game:HttpGet("https://raw.githubusercontent.com/Scriptez1/RedXRobloxScripts/main/bloxfruit.lua"))()
-     return
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Scriptez1/RedXRobloxScripts/main/bloxfruit.lua"))()
+    return
+else
+    print("Unknown")
+    return
 end
